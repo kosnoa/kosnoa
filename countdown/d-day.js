c2edddb4f8cdc486fb873972df954a6f968cb768
kosnoa <f4less0152@gmail.com>
2022-10-25 11:41:39 -0400

    Added more midterm days

--- countdown/d-day.js
+++ countdown/d-day.js
@@ -1,6 +1,27 @@
+var countDownCS135_Mid = new Date("Oct 3, 2022 19:00:00").getTime();
 var countDownMath135_Mid = new Date("Oct 17, 2022 19:00:00").getTime();
 var countDownMath137_Mid = new Date("Oct 24, 2022 19:00:00").getTime();
 var countDownCS135_Mid2 = new Date("Nov 7, 2022 19:00:00").getTime();
+var countDownMath135_Mid2 = new Date("Nov 17, 2022 19:00:00").getTime();
+
+var cs135_mid = setInterval(function() {
+    var now = new Date().getTime();
+
+    var distance = countDownCS135_Mid - now;
+
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("cs_135").innerHTML = days + "d " + hours + "h " +
+        minutes + "m " + seconds + "s ";
+
+    if (distance < 0) {
+        clearInterval(cs135_mid);
+        document.getElementById("cs_135").innerHTML = "PASSED";
+    }
+}, 1000);
 
 var math135_mid = setInterval(function() {
     var now = new Date().getTime();
@@ -12,12 +33,12 @@ var math135_mid = setInterval(function() {
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    document.getElementById("math_135").innerHTML = days + "d " + hours + "h "
-    + minutes + "m " + seconds + "s ";
-      
+    document.getElementById("math_135").innerHTML = days + "d " + hours + "h " +
+        minutes + "m " + seconds + "s ";
+
     if (distance < 0) {
-      clearInterval(math135_mid);
-      document.getElementById("math_135").innerHTML = "PASSED";
+        clearInterval(math135_mid);
+        document.getElementById("math_135").innerHTML = "PASSED";
     }
 }, 1000);
 
@@ -31,12 +52,12 @@ var math137_mid = setInterval(function() {
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    document.getElementById("math_137").innerHTML = days + "d " + hours + "h "
-    + minutes + "m " + seconds + "s ";
-      
+    document.getElementById("math_137").innerHTML = days + "d " + hours + "h " +
+        minutes + "m " + seconds + "s ";
+
     if (distance < 0) {
-      clearInterval(math137_mid);
-      document.getElementById("math_137").innerHTML = "PASSED";
+        clearInterval(math137_mid);
+        document.getElementById("math_137").innerHTML = "PASSED";
     }
 }, 1000);
 
@@ -50,11 +71,30 @@ var cs135_mid2 = setInterval(function() {
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    document.getElementById("cs_135").innerHTML = days + "d " + hours + "h "
-    + minutes + "m " + seconds + "s ";
-      
+    document.getElementById("cs_135_2").innerHTML = days + "d " + hours + "h " +
+        minutes + "m " + seconds + "s ";
+
     if (distance < 0) {
-      clearInterval(cs135_mid2);
-      document.getElementById("cs_135").innerHTML = "PASSED";
+        clearInterval(cs135_mid2);
+        document.getElementById("cs_135_2").innerHTML = "PASSED";
+    }
+}, 1000);
+
+var math135_mid2 = setInterval(function() {
+    var now = new Date().getTime();
+
+    var distance = countDownMath135_Mid2 - now;
+
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("math_135_2").innerHTML = days + "d " + hours + "h " +
+        minutes + "m " + seconds + "s ";
+
+    if (distance < 0) {
+        clearInterval(math135_mid2);
+        document.getElementById("math_135_2").innerHTML = "PASSED";
     }
 }, 1000);
