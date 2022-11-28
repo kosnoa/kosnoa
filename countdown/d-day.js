e7ac254bd8b25af02582cd5bee2f7437e01f7c20
kosnoa <f4less0152@gmail.com>
2022-11-27 19:42:45 -0500

    Added End of Semester countdown

--- countdown/d-day.js
+++ countdown/d-day.js
@@ -3,6 +3,7 @@ var countDownMath135_Mid = new Date("Oct 17, 2022 19:00:00").getTime();
 var countDownMath137_Mid = new Date("Oct 24, 2022 19:00:00").getTime();
 var countDownCS135_Mid2 = new Date("Nov 7, 2022 19:00:00").getTime();
 var countDownMath135_Mid2 = new Date("Nov 14, 2022 19:00:00").getTime();
+var countDownEndofSemester = new Date("Dec 6, 2022 23:59:59").getTime();
 
 var cs135_mid = setInterval(function() {
     var now = new Date().getTime();
@@ -96,5 +97,24 @@ var math135_mid2 = setInterval(function() {
     if (distance < 0) {
         clearInterval(math135_mid2);
         document.getElementById("math_135_2").innerHTML = "PASSED";
+    }
+}, 1000);
+
+var endofsemester = setInterval(function() {
+    var now = new Date().getTime();
+
+    var distance = countDownEndofSemester - now;
+
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("end_of_semester").innerHTML = days + "d " + hours + "h " +
+        minutes + "m " + seconds + "s ";
+
+    if (distance < 0) {
+        clearInterval(endofsemester);
+        document.getElementById("end_of_semester").innerHTML = "PASSED";
     }
 }, 1000);
