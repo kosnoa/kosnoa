9272283081d250bcbb9256bfe44457cdbfab36b0
kosnoa <f4less0152@gmail.com>
2023-03-20 22:21:04 -0400

    Changed Introduction

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var Countdown2022 = new Date ("Dec 31, 2022 23:59:59").getTime();
+var Countdown2022 = new Date("Dec 31, 2023 23:59:59").getTime();
 
 var count2022 = setInterval(function() {
     var now = new Date().getTime();
@@ -10,12 +10,12 @@ var count2022 = setInterval(function() {
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    document.getElementById("2022_count").innerHTML = days + "d " + hours + "h " +
-    minutes + "m " + seconds + "s ";
+    document.getElementById("2023_count").innerHTML = days + "d " + hours + "h " +
+        minutes + "m " + seconds + "s ";
 
-if (distance < 0) {
-    clearInterval(count2022);
-    document.getElementById("2022_count").innerHTML = "PASSED";
-}
+    if (distance < 0) {
+        clearInterval(count2022);
+        document.getElementById("2023_count").innerHTML = "PASSED";
+    }
 
 }, 1000);
